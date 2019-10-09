3ac1e08e153c8145267d65dfd39e20b4e657ec29
Brighton <cbrghton@gmail.com>
2019-10-09 15:22:44 -0500

    Remove scripts
    Former-commit-id: 9e51a857b593cc65e068be6ac0986cc7c53f1181

--- scripts/instantclient_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-ln -s /usr/local/instantclient_10.1/ /usr/local/instantclient
-echo /usr/local/instantclient_10.1/libclntsh.so.10.1 /usr/local/instantclient/libclntsh.so
-ln -s /usr/local/instantclient_10.1/libclntsh.so.10.1 /usr/local/instantclient/libclntsh.so
-ln -s /usr/local/instantclient_10.1/sqlplus /usr/bin/sqlplus
-
-echo 'instantclient,/usr/local/instantclient' | pecl install oci8
-echo "extension=oci8.so" > /usr/local/etc/php/conf.d/php-oci8.ini
